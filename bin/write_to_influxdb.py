3f18f649b27fcce340042b026b724700fd69dd70
Ben Forsyth <bforsyth@antiflux.org>
2015-10-25 21:50:52 -0700

    basic influxdb database test

--- bin/write_to_influxdb.py
+++ bin/write_to_influxdb.py
@@ -11,12 +11,25 @@ import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from RainEagle import Eagle, to_epoch_1970
-import time
+
 import argparse
+from influxdb import InfluxDBClient
 from pprint import pprint
+import time
+
+def write_demand_values(db):
+    print "hi!"
 
 def main():
-    exit(1)
+    db = InfluxDBClient()
+    databases = [x["name"] for x in db.get_list_database()]
+    if "rain_eagle" in databases:
+        write_demand_values(db)
+    else:
+        print "Did not find database with name \"rain_eagle\""
+        exit(-1)
+
+    exit(0)
 
 if __name__ == "__main__":
     main()
