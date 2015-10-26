fc6afd049d8a04cac9f294ee4d84df5db3dddb2b
Ben Forsyth <bforsyth@antiflux.org>
2015-10-25 21:36:57 -0700

    empty script template

---
+++ bin/influxdb.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+"""
+    Read data from eagle and write to influxdb database
+"""
+
+__author__ = "Ben Forsyth"
+__version__ = "0.1"
+
+# import RainEagle
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from RainEagle import Eagle, to_epoch_1970
+import time
+import argparse
+from pprint import pprint
+
+def main():
+    exit(1)
+
+if __name__ == "__main__":
+    main()
+    exit(0)
+
