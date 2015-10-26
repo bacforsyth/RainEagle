fdc342fce0d6494ea2347d447fcda01836d3c1ed
Ben Forsyth <bforsyth@antiflux.org>
2015-10-25 22:16:03 -0700

    series helper and an eagle

--- bin/write_to_influxdb.py
+++ bin/write_to_influxdb.py
@@ -13,18 +13,55 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from RainEagle import Eagle, to_epoch_1970
 
 import argparse
-from influxdb import InfluxDBClient
+from influxdb import InfluxDBClient, SeriesHelper
 from pprint import pprint
 import time
 
-def write_demand_values(db):
+debug = 0
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+                    description="write data to influxdb")
+
+    parser.add_argument("-a", "--address", dest="addr",
+                    default=os.getenv('EAGLE_ADDR', None),
+                    help="hostname or IP device")
+
+    parser.add_argument("-p", "--port", dest="port", type=int,
+                    default=os.getenv('EAGLE_PORT', 5002),
+                    help="command socket port")
+
+    parser.add_argument("-d", "--debug", dest="debug",
+                    default=debug, action="count",
+                    help="print debug info")
+
+    parser.add_argument("-m", "--mac", dest="mac",
+                    help="Eagle radio mac addrress")
+
+    return parser
+
+def write_demand_values(eagle, db):
+    class MySeriesHelper(SeriesHelper):
+        # Meta class stores time series helper configuration.
+        class Meta:
+            client = db
+            series_name = 'demand'
+            fields = ['kW']
+            bulk_size = 5
+            autocommit = True
+    # TODO: get latest demand value to filter our data with
     print "hi!"
 
 def main():
+    parser = create_parser()
+    args = parser.parse_args()
+    print args
+
     db = InfluxDBClient()
     databases = [x["name"] for x in db.get_list_database()]
     if "rain_eagle" in databases:
-        write_demand_values(db)
+        eg = Eagle(**vars(args))
+        write_demand_values(eg, db)
     else:
         print "Did not find database with name \"rain_eagle\""
         exit(-1)
